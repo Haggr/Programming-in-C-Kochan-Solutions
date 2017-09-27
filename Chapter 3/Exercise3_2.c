d6341bd4a3a77d7aaf48d984784aaa7c4530fc25
Hasan Buğra Arslan <hasanbugra333@gmail.com>
2017-09-27 14:50:05 +0300

    Add files via upload

---
+++ Chapter 3/Exercise3_2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+   printf("In C, lowercase letters are significant.\n"
+
+          "main is where program execution begins.\n"
+
+          "Opening and closing braces enclose program"
+          " statements in a routine.\n"
+
+          "All program statements must be terminated by a"
+          " semicolon.\n");
+
+   return 0;
+}
---
+++ Chapter 3/Exercise3_4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+   int op1 = 87, op2 = 15;
+
+   printf("%i - %i = %i\n", op1, op2, op1 - op2);
+
+   return 0;
+}
